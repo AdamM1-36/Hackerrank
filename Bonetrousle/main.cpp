8049fc3e9c9dd3a88c37ac267b2e31e335aeb941
AdamM1-36 <72456013+AdamM1-36@users.noreply.github.com>
2022-03-12 23:21:03 +0700

    Create main.cpp

---
+++ Bonetrousle/main.cpp
@@ -0,0 +1,99 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+string ltrim(const string &);
+string rtrim(const string &);
+vector<string> split(const string &);
+
+vector<long> bonetrousle(long n, long k, int b) {
+    vector<long> ans(b,0);
+    long lb=b;
+    long min = lb*(lb+1)/2;
+    long q = (n-min)/b, r = (n-min)%b;
+    if(n<min) return vector<long>(1,-1);
+    iota(ans.begin(), ans.end(), 1);
+    for(long i=0; i<b; i++){
+        i<(b-r)?ans[i]+=q:ans[i]+=(q+1);
+    }
+    if(ans[b-1]>k) return vector<long>(1,-1);
+    return ans;
+}
+
+int main()
+{
+    ofstream fout(getenv("OUTPUT_PATH"));
+
+    string t_temp;
+    getline(cin, t_temp);
+
+    int t = stoi(ltrim(rtrim(t_temp)));
+
+    for (int t_itr = 0; t_itr < t; t_itr++) {
+        string first_multiple_input_temp;
+        getline(cin, first_multiple_input_temp);
+
+        vector<string> first_multiple_input = split(rtrim(first_multiple_input_temp));
+
+        long n = stol(first_multiple_input[0]);
+
+        long k = stol(first_multiple_input[1]);
+
+        int b = stoi(first_multiple_input[2]);
+
+        vector<long> result = bonetrousle(n, k, b);
+
+        for (size_t i = 0; i < result.size(); i++) {
+            fout << result[i];
+
+            if (i != result.size() - 1) {
+                fout << " ";
+            }
+        }
+
+        fout << "\n";
+    }
+
+    fout.close();
+
+    return 0;
+}
+
+string ltrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        s.begin(),
+        find_if(s.begin(), s.end(), not1(ptr_fun<int, int>(isspace)))
+    );
+
+    return s;
+}
+
+string rtrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        find_if(s.rbegin(), s.rend(), not1(ptr_fun<int, int>(isspace))).base(),
+        s.end()
+    );
+
+    return s;
+}
+
+vector<string> split(const string &str) {
+    vector<string> tokens;
+
+    string::size_type start = 0;
+    string::size_type end = 0;
+
+    while ((end = str.find(" ", start)) != string::npos) {
+        tokens.push_back(str.substr(start, end - start));
+
+        start = end + 1;
+    }
+
+    tokens.push_back(str.substr(start));
+
+    return tokens;
+}
